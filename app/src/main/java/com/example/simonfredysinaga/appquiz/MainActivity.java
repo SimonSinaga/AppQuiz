5436c16db605e7c54c8d3d4565f77fa16f0135c3
SimonSinaga <simonfredy12@gmail.com>
2019-04-08 14:35:49 +0700

    arigatou

--- app/src/main/java/com/example/simonfredysinaga/appquiz/MainActivity.java
+++ app/src/main/java/com/example/simonfredysinaga/appquiz/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity  {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent home = new Intent(MainActivity.this, home.class);
+                Intent home = new Intent(MainActivity.this, user.class);
                 startActivity(home);
                 finish();
             }
--- app/src/main/java/com/example/simonfredysinaga/appquiz/user.java
+++ app/src/main/java/com/example/simonfredysinaga/appquiz/user.java
@@ -2,12 +2,23 @@ package com.example.simonfredysinaga.appquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class user extends AppCompatActivity {
+
+    EditText isinama, isiemail, isiusername, isipassword;
+    Button btndaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        isinama = (EditText)findViewById(R.id.isi_nama);
+        isiemail = (EditText)findViewById(R.id.isi_email);
+        isiusername = (EditText)findViewById(R.id.isi_username);
+        isipassword = (EditText)findViewById(R.id.isi_password);
+        btndaftar = (Button)findViewById(R.id.bt_daftar);
     }
 }

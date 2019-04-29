4e7f279e581e0a1db734a2f51c59b9fcaf50cafd
SimonSinaga <simonfredy12@gmail.com>
2019-04-29 20:49:54 +0700

    perubahan sudah bisa register

--- app/src/main/java/com/example/simonfredysinaga/appquiz/Database.java
+++ app/src/main/java/com/example/simonfredysinaga/appquiz/Database.java
@@ -1,19 +1,24 @@
 package com.example.simonfredysinaga.appquiz;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
 
     public static final String NAMA_DB = "dbkuis";
-    public static final String NAMA_TB = "tb_admin";
+    public static final String NAMA_TB = "tbadmin";
     public static final int DB_VER = 1;
 
     public static final String ID_ADMIN = "id_admin";
     public static final String NAMA_ADMIN = "nama_admin";
     public static final String USERNAME_ADMIN = "username_admin";
-    public static final String PASSWORD_ADMIN = "password_ADMIN";
+    public static final String PASSWORD_ADMIN = "password_admin";
 
     public static final String CREATE_TB = "create table "+NAMA_TB+
             "("+ID_ADMIN + " integer primary key autoincrement, "+
@@ -46,6 +51,34 @@ public class Database {
             db.execSQL("DROP TABLE IF EXISTS "+NAMA_TB);
             onCreate(db);
         }
+    }
+
+    public void sisipData(String namaAdm, String usernameAdm, String passwordAdm){
+        ContentValues cv = new ContentValues();
+        cv.put(NAMA_ADMIN, namaAdm);
+        cv.put(USERNAME_ADMIN, usernameAdm);
+        cv.put(PASSWORD_ADMIN, passwordAdm);
+        sqLiteDatabase.insert(NAMA_TB, null, cv);
+    }
+
+
+
+    public ArrayList<HashMap<String, String>> tampilData(){
+        ArrayList<HashMap<String, String>> dataList;
+        dataList = new ArrayList<HashMap<String, String>>();
+        Cursor cursor = sqLiteDatabase.rawQuery(
+                "select * from "+NAMA_TB, null);
+        if(cursor.moveToFirst()){
+            do{
+                HashMap<String, String> data = new HashMap<String, String>();
+                data.put(ID_ADMIN, cursor.getString(0));
+                data.put(NAMA_ADMIN, cursor.getString(1));
+                data.put(USERNAME_ADMIN, cursor.getString(2));
+                data.put(PASSWORD_ADMIN, cursor.getString(3));
+                dataList.add(data);
+            }while (cursor.moveToNext());
+        }
+        return dataList;
     }
 
 }
